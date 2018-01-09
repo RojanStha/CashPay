9ec7a67479b36771a5111ad54e74f9345fb2ab36
SwetaShrestha <soyetastha@gmail.com>
2018-01-09 13:18:47 +0545

    Initial Commit

---
+++ topUpNpTests/topUpNpTests.swift
@@ -0,0 +1,36 @@
+//
+//  topUpNpTests.swift
+//  topUpNpTests
+//
+//  Created by Rojan on 1/9/2561 BE.
+//  Copyright © 2561 Rojan. All rights reserved.
+//
+
+import XCTest
+@testable import topUpNp
+
+class topUpNpTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
